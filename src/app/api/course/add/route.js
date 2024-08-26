097840a12bc548ace0441c7631f56785d1fd8427
nelsonlobo99 <nelsonlobo@protonmail.com>
2024-08-26 21:59:02 +0530

    Image Upload To CDN

--- src/app/api/course/add/route.js
+++ src/app/api/course/add/route.js
@@ -1,17 +1,32 @@
 import { PrismaClient } from "@prisma/client";
+import { put } from '@vercel/blob';
 
 const prisma = new PrismaClient();
 
 export async function POST(req) {
     const data = await req.formData();
     try{
-        const response_data = await prisma.courses.create({
+        const file = data.get("file");
+        const fileName = file.name
+
+        const blob = await put(fileName, file, {
+            access: 'public',
+        });
+
+        const db_response = await prisma.courses.create({
             data: {
                 name: data.get("courseName"),
                 description: data.get("description"),
-                coverImage: ''
+                coverImage: JSON.stringify(blob)
             }
         })
+
+        const response_data = {
+            courseId: db_response.id,
+            courseName: db_response.name,
+            description: db_response.description,
+            coverImage: JSON.parse(db_response.coverImage).url
+        }
         return Response.json({data: response_data}, {status: 200});
     }catch(error){
         console.log(error)
